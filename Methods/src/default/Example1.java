aa8cd994fe0097221291398acd19ab7850918505
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-10-09 03:08:26 -0700

    adding java method lessons

---
+++ Methods/src/default/Example1.java
@@ -0,0 +1,9 @@
+public class Example1 {
+    public static void main(String[] args) {
+        sayHelloMethod();
+        sayHelloMethod();
+    }
+    public static void sayHelloMethod() {
+        System.out.println("Hello");
+    }
+}
