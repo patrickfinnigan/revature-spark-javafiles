81e7f37cc228c4eaf791cf21e06ebc198f7b8c35
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-10-09 04:15:45 -0700

    adding method parameters exercise

---
+++ MethodParameters/src/default/Example1.java
@@ -0,0 +1,11 @@
+public class Example1 {
+    public static void main(String[] args) {
+        printName("Patrick");
+        printName("Mark");
+    }
+
+    public static void printName(String name) {
+        System.out.println("Hello " + name);
+    }
+
+}
